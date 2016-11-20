96adba85c7c0952c6ca8aec5e4a9607087f6cace
Eung Jin Lee <oiojin831@gmail.com>
2016-11-20 12:40:58 +0900

    new constructor for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: "./entry.js",
+  entry: {
+    app: './app',
+  },
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: '[name].js',
+    chunkFileName: '[id].js',
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css" },
-      { 
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'stage-0', 'react'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
-  }
+          plugins: ['transform-runtime'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css', {
+      allChunks: true,
+    }),
+  ],
 };
