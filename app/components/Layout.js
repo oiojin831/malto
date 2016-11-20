688ee55676cef5f8bce42c215104b427c0471c36
Eung Jin Lee <oiojin831@gmail.com>
2016-11-20 17:50:12 +0900

    add click evnent

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -4,12 +4,21 @@ import Contact from './Contact';
 import data from './data';
 
 class Layout extends React.Component {
+  addContact = (e) => {
+    //default 행동을 막는다 여기서 #로 이동
+    e.preventDefault()
+    console.log('clicked!');
+  }
+
   render() {
     return (
-      <div id='Layout' className='pure-g'>
-        { data.map(info =>
-          <Contact key={info.id} {...info} />
-        )}
+      <div id='Layout'>
+        <a href='#' className='pure-button' onClick={this.addContact}>Add Button</a>
+        <div className='pure-g'>
+          { data.map(info =>
+            <Contact key={info.id} {...info} />
+          )}
+        </div>
       </div>
     );
   }
--- app/components/data.js
+++ app/components/data.js
@@ -1,7 +1,7 @@
 const data = [
-  { name: 'eung jin', email: 'oiojin@gmail.com' },
-  { name: 'jeff', email: 'jeff@gmail.com' },
-  { name: 'allison', email: 'allison@gmail.com' },
+  { id: 1, name: 'eung jin', email: 'oiojin@gmail.com' },
+  { id: 2, name: 'jeff', email: 'jeff@gmail.com' },
+  { id: 3, name: 'allison', email: 'allison@gmail.com' },
 ];
 
 export default data;
